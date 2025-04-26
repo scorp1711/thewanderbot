f866b93d3ccf3d83784a859a18dd8632c03dfc52
scorp1711 <sivaranjani1711@gmail.com>
2025-04-26 11:58:10 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ server.listen(process.env.PORT || 3978, function () {
     console.log(`\n${server.name} listening to ${server.url}`);
 });
 
+// ➡️ New root route
+server.get('/', (req, res, next) => {
+  res.send(200, 'Wanderbot App is Running!');
+  next();
+});
+
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
@@ -18,6 +24,5 @@ server.post('/api/messages', async (req, res, next) => {
     await adapter.processActivity(req, res, async (context) => {
         await bot.run(context);
     });
-    next(); // Important!
+    next();
 });
-
