c27f4089db74363a033a17359ffdb29eef3edbb1
scorp1711 <sivaranjani1711@gmail.com>
2025-04-26 05:27:45 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,25 @@
 const { ActivityHandler } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
-    constructor() {
-        super();
-        this.onMessage(async (context, next) => {
-            const text = context.activity.text;
-            await context.sendActivity(`You said: ${text}`);
-            await next();
-        });
+  constructor() {
+    super();
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            for (let member of membersAdded) {
-                if (member.id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome to WanderBot!');
-                }
-            }
-            await next();
-        });
-    }
+    this.onMessage(async (context, next) => {
+      const text = context.activity.text;
+      await context.sendActivity(`You said: ${text}`);
+      await next();
+    });
+
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let member of membersAdded) {
+        if (member.id !== context.activity.recipient.id) {
+          await context.sendActivity('Hello and welcome to WanderBot!');
+        }
+      }
+      await next();
+    });
+  }
 }
 
-module.exports.EchoBot = EchoBot;
+module.exports = { EchoBot };
